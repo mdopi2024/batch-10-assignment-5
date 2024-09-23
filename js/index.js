c2b0c6a3016da8c09add15de3cb2616960026383
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-09-23 16:14:56 +0600

    almost done assignmetn 5

--- js/index.js
+++ js/index.js
@@ -5,27 +5,85 @@ document.getElementById('donate-now-button').addEventListener('click',function()
     const navBdt = convertNumber2('nav-totoal-bdt')
     const noakaliBdt = convertNumber2('noakali-bdt')
   if(typeof numberValue === 'number' || inputValue >= 0){
-
    const noakaliTotal = inputValue +noakaliBdt;
    document.getElementById('noakali-bdt').innerText = noakaliTotal
    const navValue= navBdt-inputValue;
    document.getElementById('nav-totoal-bdt').innerText= navValue
-   
+   const time =new Date()
    const div = document.createElement('div')
+    div.classList.add('border')
+    div.classList.add('mt-3')
    div.innerHTML +=`
-    <p >${inputValue} Taka is Donated for famine-2024 at Feni, Bangladesh </p>
+    <p >${inputValue} Taka is Donated for famine-2024 at Noakhali, Bangladesh </p>
     <p >${time} </p>
     `
  const history = document.getElementById('history-section')
- history.appendChild(div)
-   
-    
+ history.appendChild(div) 
   }
   else{
     alert("Please Enter  Valid Number")
   }  
   
 })
+
+
+// feni blood section here start
+document.getElementById('donate-now-button-feni').addEventListener('click',function(){
+  const inputValue= convertNumber('input-feni')
+  const navBdt = convertNumber2('nav-totoal-bdt')
+  const feniBdt = convertNumber2('feni-bdt')
+  if(typeof numberValue === 'number' || inputValue >= 0){
+   const feniTotal = feniBdt + inputValue;
+   document.getElementById('feni-bdt').innerText= feniTotal
+  const navTotal = navBdt - inputValue
+  document.getElementById('nav-totoal-bdt').innerText = navTotal
+
+  const time =new Date()
+  const div = document.createElement('div')
+   div.classList.add('border')
+   div.classList.add('mt-3')
+  div.innerHTML +=`
+   <p >${inputValue} Taka is Donated for Flood Relief in Feni,Bangladesh </p>
+   <p >${time} </p>
+   `
+const history = document.getElementById('history-section')
+history.appendChild(div)
+  }
+  else{
+    alert("Please Enter  Valid Number")
+  } 
+
+})
+// feni blood section here end
+
+
+// movement injorieded start 
+document.getElementById('donate-now-button-movment').addEventListener('click',function(){
+  const inputValue= convertNumber('input-movment')
+  const navBdt = convertNumber2('nav-totoal-bdt')
+  const movmentBdt = convertNumber2('movment-bdt')
+  if(typeof numberValue === 'number' || inputValue >= 0){
+    const movementTotal = inputValue + movmentBdt
+    document.getElementById('movment-bdt').innerText = movementTotal;
+    const navTotal = navBdt - inputValue;
+    document.getElementById('nav-totoal-bdt').innerText = navTotal;
+    const time =new Date()
+  const div = document.createElement('div')
+   div.classList.add('border')
+   div.classList.add('mt-3')
+  div.innerHTML +=`
+   <p >${inputValue} Taka is Donated for famine-2024 at movment injoried, Bangladesh </p>
+   <p >${time} </p>
+   `
+const history = document.getElementById('history-section')
+history.appendChild(div) 
+  }
+  else{
+    alert("Please Enter  Valid Number")
+  }  
+})
+
+// movement injorieded end
 
 
 // swich donate and history button
