c1acbdd8d3377608af866012b69f61a202a548e5
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-09-23 13:29:10 +0600

    first section almost done

---
+++ js/common.js
@@ -0,0 +1,23 @@
+function convertNumber (id){
+    const value = document.getElementById(id).value;
+    const valueNumber = parseFloat(value)
+    return valueNumber;
+}
+
+// texarea
+function convertNumber2 (id){
+    const value1 = document.getElementById(id).innerText;
+    const valueNumber1 = parseFloat(value1)
+    return valueNumber1;
+}
+
+
+
+function donationHistory (id){
+    document.getElementById('history-section').classList.add('hidden')
+    document.getElementById('donation-section').classList.add('hidden')
+
+    // remove
+
+    document.getElementById(id).classList.remove('hidden')
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-console.log('hello')
+
+
+document.getElementById('donate-now-button').addEventListener('click',function(){
+    const inputValue= convertNumber('input')
+    const navBdt = convertNumber2('nav-totoal-bdt')
+    const noakaliBdt = convertNumber2('noakali-bdt')
+  if(typeof numberValue === 'number' || inputValue >= 0){
+
+   const noakaliTotal = inputValue +noakaliBdt;
+   document.getElementById('noakali-bdt').innerText = noakaliTotal
+   const navValue= navBdt-inputValue;
+   document.getElementById('nav-totoal-bdt').innerText= navValue
+   
+   const div = document.createElement('div')
+   div.innerHTML +=`
+    <p >${inputValue} Taka is Donated for famine-2024 at Feni, Bangladesh </p>
+    <p >${time} </p>
+    `
+ const history = document.getElementById('history-section')
+ history.appendChild(div)
+   
+    
+  }
+  else{
+    alert("Please Enter  Valid Number")
+  }  
+  
+})
+
+
+// swich donate and history button
+document.getElementById('click-donate').addEventListener('click',function(){
+donationHistory('donation-section')
+})
+document.getElementById('click-history').addEventListener('click',function(){
+donationHistory('history-section')
+})
